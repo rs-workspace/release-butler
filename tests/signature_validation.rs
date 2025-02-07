ef180c0a6c940c58b2f423da9f3fa2a64791ee9e
Aditya Kumar <git@adityais.dev>
2025-02-07 21:11:03 +0000

    Add test for failed GitHub webhook signature validation

--- tests/signature_validation.rs
+++ tests/signature_validation.rs
@@ -28,6 +28,26 @@ async fn test_empty_github_event_header() {
 }
 
 #[actix_web::test]
+async fn test_fail_signature_validation() {
+    let signature_header = tests_utils::TestHeader("x-hub-signature-256", "sha256=123".into());
+
+    let event_header = tests_utils::TestHeader("x-github-event", "issues".into());
+
+    let req = tests_utils::test::TestRequest::post()
+        .uri(tests_utils::WEBHOOK_ENDPOINT)
+        .set_payload(r"Hello World!")
+        .insert_header(signature_header)
+        .insert_header(event_header)
+        .to_request();
+    let resp = tests_utils::test_endpoint(req).await;
+
+    assert_eq!(resp.status(), StatusCode::UNAUTHORIZED);
+
+    let body = tests_utils::test::read_body(resp).await;
+    assert_eq!(body, WebhookError::InvalidSignature.to_bytes());
+}
+
+#[actix_web::test]
 async fn test_signature_validation() {
     // The default key is "abc"
     let signature_header = tests_utils::TestHeader(
